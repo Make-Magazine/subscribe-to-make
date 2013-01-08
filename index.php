25fe7bc762d0061fbb6b4976666dc681d891030a
Jake Spurlock <whyisjake@gmail.com>
2013-01-07 16:56:18 -0800

    Updating the end of year ads.

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 </head>
 <body>
 
-	<a target="_blank" href="https://readerservices.makezine.com/MK/subgift.aspx?PC=MK&PK=44CBAN7&FC=UNITED+STATES">
+	<a target="_blank" href="https://readerservices.makezine.com/MK/subgift.aspx?PC=MK&PK=44CBAN9&FC=UNITED+STATES">
 		<img src="img/subscribe.jpg" alt="Subscribe to MAKE">
 	</a>
 	
